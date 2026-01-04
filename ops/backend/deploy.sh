af8dea4e5f50144c8195eafcc52a03e7084b7108
Maciej Kedzior <m.kedzior@onet.pl>
2026-01-04 13:43:50 +0100

    turn off verbose curl

--- ops/backend/deploy.sh
+++ ops/backend/deploy.sh
@@ -9,5 +9,5 @@ if [[ -z "${RENDER_BACKEND_DEV_HOOK:-}" ]]; then
   exit 1
 fi
 
-curl -fsSv -X POST "$RENDER_BACKEND_DEV_HOOK"
+curl -fsS -X POST "$RENDER_BACKEND_DEV_HOOK"
 echo "Triggered backend DEV deploy on Render"
